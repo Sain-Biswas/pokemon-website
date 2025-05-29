5b0a46f79995d0ca455a0ce7022a3b15337ce62d
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-29 16:38:24 +0530

    feat: add env validation using zod/v4

---
+++ src/constant/env.ts
@@ -0,0 +1,19 @@
+import { z } from "zod/v4";
+
+const environmentVariablesSchema = z.object({
+  DATABASE_URL: z.string(),
+  NODE_ENV: z.enum(["development", "production", "testing"]),
+});
+
+const envParsed = environmentVariablesSchema.safeParse(process.env);
+
+if (!envParsed.success) {
+  console.error(envParsed.error);
+  process.exit(1);
+}
+
+const environmentVariables = envParsed.data;
+
+export default environmentVariables;
+
+export type TEnvironmentVariables = z.infer<typeof environmentVariablesSchema>;
