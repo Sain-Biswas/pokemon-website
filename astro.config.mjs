3e27a29099c411ee2f66d887d7d0db8dbc35527a
Sain-Biswas <sain.biswas614@gmail.com>
2025-08-23 21:09:37 +0530

    build: add astro integrations - vercel, mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel({
+    isr: {
+      expiration: false,
+    },
+  }),
+  integrations: [mdx({
+    shikiConfig: {
+      theme: "rose-pine",
+    },
+    gfm: true,
+    syntaxHighlight: {
+      type: 'shiki',
+    }
+  })]
+});
